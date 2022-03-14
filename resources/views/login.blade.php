495f23c8b61c9e50e891f542817055844960ada0
smeeksha <er.sharma2k14@gmail.com>
2022-03-14 16:23:23 +0530

    first commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,13 +3,13 @@
 <div class="container custom-login">
     <div class="row" style="margin-left:300px";>
         <div class="col-sm-8 col-sm-offset-4">
-            <form>
+            <form action="login" method="POST">
                 <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
                     <input type="email" class="form-control" id="email" style="width:100%" name="email">
                 </div>
                 <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password" class="form-control" id="password" style="width:100%" name="password">
                 </div>
             <button type="submit" class="btn btn-primary">Submit</button>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
+
+Route::post("/login",[UserController::class,'login']);
